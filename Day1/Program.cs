31b8ce5114f6c9ac4126d290b23a9832aeb80509
Michael R-P <michael.r-p@ecstech.com>
2022-12-01 08:33:58 -0500

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+Console.WriteLine($"{lines.Length} lines in file");
+
+var current = 0;
+var sums = new List<int>();
+
+foreach (var line in lines)
+{
+    if (int.TryParse(line, out var amt))
+    {
+        current += amt;
+    }
+    else
+    {
+        sums.Add(current);
+        current = 0;
+    }
+}
+
+var max = sums.OrderDescending().First();
+var top3 = sums.OrderDescending().Take(3).Sum();
+
+Console.WriteLine($"max {max}, top3 = {top3}");
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AdventOfCode2022
+# Advent Of Code 2022
+
+[Advent of Code 2022](https://adventofcode.com/2022)
