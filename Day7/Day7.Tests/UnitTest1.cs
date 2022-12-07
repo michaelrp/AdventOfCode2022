c1917366e3c2e3717d3a988864b9e947460810f0
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-12-07 00:30:41 -0500

    Set up Day7

---
+++ Day7/Day7.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Day7.Tests;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,3 @@
+﻿var lines = File.ReadAllLines("../Day7/input.txt");
+
+Console.WriteLine($"{lines.Length} lines in file");
