5ab0d4e9d945a72974312a8392e21bc81ae1dc13
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-12-09 17:49:14 -0500

    Part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,15 +1,21 @@
-﻿var lines = File.ReadAllLines("input.txt");
+﻿using System.Text;
+
+var lines = File.ReadAllLines("input.txt");
 Console.WriteLine($"{lines.Length} lines in file");
 
-var hx = 0;
-var hy = 0;
-var tx = 0;
-var ty = 0;
+var minhx = 0;
+var maxhx = 0;
+var minhy = 0;
+var maxhy = 0;
 
+var knotCount = 10;
+var knots = new int[knotCount, knotCount];
+
+// use a simple HashSet to keep track of visited
 var visited = new HashSet<string>();
 visited.Add("0:0");
 
-foreach(var move in lines)
+foreach (var move in lines)
 {
     var parts = move.Split(" ");
     var dir = parts[0];
@@ -17,37 +23,106 @@ foreach(var move in lines)
 
     for (int i = 0; i < steps; i++)
     {
+        // move head
         if (dir == "U")
-            hy++;
+            knots[0, 1]++;
         else if (dir == "D")
-            hy--;
+            knots[0, 1]--;
         else if (dir == "L")
-            hx--;
+            knots[0, 0]--;
         else // R
-            hx++;
-        
-        // check x distance
-        if (Math.Abs(hx - tx) > 1)
-        {
-            tx = dir == "L" ? hx + 1 : hx - 1;
-            ty = hy;
-        }
+            knots[0, 0]++;
 
-        // check y distance
-        if (Math.Abs(hy - ty) > 1)
-        {
-            ty = dir == "U" ? hy - 1 : hy + 1;
-            tx = hx;
-        }
+        minhx = knots[0, 0] < minhx ? knots[0, 0] : minhx;
+        maxhx = knots[0, 0] > maxhx ? knots[0, 0] : maxhx;
+        minhy = knots[0, 1] < minhy ? knots[0, 1] : minhy;
+        maxhy = knots[0, 1] > maxhy ? knots[0, 1] : maxhy;
 
-        visited.Add($"{tx}:{ty}");
+        // move tails
+        MoveTail(0, 1);
 
-        // Console.WriteLine($"h ({hx}, {hy}), t ({tx},{ty})");
     }
-
+    // ViewPosition();
 }
 
 // Simulate your complete hypothetical series of motions.
 // How many positions does the tail of the rope visit at least once?
 
-Console.WriteLine($"visited count {visited.Count}");
+// Simulate your complete series of motions on a larger rope with ten knots.
+// How many positions does the tail of the rope visit at least once?
+
+Console.WriteLine($"visited count {visited.Count} with {knotCount} knots");
+
+void MoveTail(int h, int t)
+{
+    var hx = knots[h, 0];
+    var hy = knots[h, 1];
+    var tx = knots[t, 0];
+    var ty = knots[t, 1];
+
+    // get distances
+    var dx = Math.Abs(hx - tx);
+    var dy = Math.Abs(hy - ty);
+
+    // Console.WriteLine($"h {h} t {t} hx {hx} hy {hy} tx {tx} ty {ty} dx {dx} dy {dy}");
+
+    // I had this as nested ifs, but refactored to switch with pattern
+    // matching. Dense but very clean.
+    var p = (dx, dy) switch
+    {
+        (2, 0) => (hx < tx ? hx + 1 : hx - 1, ty),
+        (0, 2) => (tx, hy < ty ? hy + 1 : hy - 1),
+        (2, 2) => (hx < tx ? hx + 1 : hx - 1, hy < ty ? hy + 1 : hy - 1),
+        (2, 1) => (hx < tx ? hx + 1 : hx - 1, hy),
+        (1, 2) => (hx, hy < ty ? hy + 1 : hy - 1),
+        _ => (tx, ty)
+    };
+
+    knots[t, 0] = p.Item1;
+    knots[t, 1] = p.Item2;
+
+    if (t + 1 < knotCount)
+        MoveTail(t, t + 1);
+    else
+        visited.Add($"{knots[t, 0]}:{knots[t, 1]}");
+}
+
+// useful for examining locations
+void ViewPosition()
+{
+    Console.WriteLine("");
+
+    var gw = Math.Abs(maxhx - minhx) + 1;
+    var gh = Math.Abs(maxhy - minhy) + 1;
+    var xoffset = Math.Abs(minhx);
+    var yoffset = Math.Abs(minhy);
+
+    var grid = new string[gw, gh];
+
+    // make a grid
+    for (int x = 0; x < gw; x++)
+        for (int y = 0; y < gh; y++)
+            grid[x, y] = ".";
+
+    // display knots in reverse order (for stacking)
+    for (int i = knotCount - 1; i >= 0; i--)
+    {
+        var x = knots[i, 0];
+        var y = knots[i, 1];
+        var display = i == 0 ? "H" : i == (knotCount - 1) ? "T" : i.ToString();
+
+        grid[xoffset, yoffset] = "s"; // starting point
+        
+        grid[x + xoffset, y + yoffset] = display;
+    }
+
+    for (int y = gh - 1; y >= 0; y--)
+    {
+        var sb = new StringBuilder();
+
+        for (int x = 0; x < gw; x++)
+            sb.Append(grid[x, y]);
+
+        Console.WriteLine(sb.ToString());
+    }
+}
