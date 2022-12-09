1940087df738d56c5636334c5dacbd3e7cf27318
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-12-09 07:53:18 -0500

    Initial

---
+++ Day9/Program.cs
@@ -0,0 +1,47 @@
+﻿var lines = File.ReadAllLines("input.txt");
+Console.WriteLine($"{lines.Length} lines in file");
+
+var hx = 0;
+var hy = 0;
+var tx = 0;
+var ty = 0;
+
+var visited = new HashSet<string>();
+
+foreach(var move in lines)
+{
+    var parts = move.Split(" ");
+    var dir = parts[0];
+    var steps = int.Parse(parts[1]);
+
+    for (int i = 0; i < steps; i++)
+    {
+        if (dir == "U")
+            hy++;
+        else if (dir == "D")
+            hy--;
+        else if (dir == "L")
+            hx--;
+        else // R
+            hx++;
+        
+        // check x distance
+        if (Math.Abs(hx - tx) > 1)
+        {
+            tx = dir == "L" ? hx + 1 : hx - 1;
+        }
+
+        // check y distance
+        if (Math.Abs(hy - ty) > 1)
+        {
+            ty = dir == "U" ? hy - 1 : hy + 1;
+        }
+
+        visited.Add($"{tx}:{ty}");
+    }
+}
+
+// Simulate your complete hypothetical series of motions.
+// How many positions does the tail of the rope visit at least once?
+
+Console.WriteLine($"visited count {visited.Count}");
