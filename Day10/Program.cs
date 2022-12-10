0e8ec7933a86746ec526777ec4eee46507a91aa5
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-12-10 14:23:35 -0500

    Day 10 answer 2

---
+++ Day10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+var lines = File.ReadAllLines("input.txt");
+
+var X = 1;
+var cycle = 0;
+
+var display = new StringBuilder(40);
+var linePosition = 0;
+
+foreach (var line in lines)
+{
+    cycle++;
+    DrawPixel();
+
+    if (line.StartsWith("addx"))
+    {
+        cycle++;
+        DrawPixel();
+
+        var xvalue = int.Parse(line.Substring(5, line.Length - 5));
+        X += xvalue;
+    }
+}
+
+void DrawPixel()
+{
+    // if the sprite overlaps the line position (sprite is X - 1, X or X + 1)
+    //     draw # else draw .
+    var pixel = linePosition >= X - 1 && linePosition <= X + 1 ? "#" : ".";
+    display.Append(pixel);
+
+    if (cycle % 40 == 0)
+    {
+        Console.WriteLine(display.ToString());
+        display.Clear();
+        linePosition = 0;
+    }
+    else
+    {
+        linePosition++;
+    }
+}
+
+// output when using 'testinput.txt'
+/*
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+*/
---
+++ Day10/README.md
@@ -0,0 +1,4 @@
+# Day 10
+
+I didn't save the "signal check" for the first part of the answer, but basically you can
+replace calls to `DrawPixel`.
