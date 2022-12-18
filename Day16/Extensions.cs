ccb5f0aa2171d7e71252d7fa0ff72057fb5ec402
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-12-18 14:20:11 -0500

    Add combinations extension

---
+++ Day16/Extensions.cs
@@ -0,0 +1,11 @@
+namespace Day16;
+
+public static class Extensions
+{
+    public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+    {
+        return k == 0 ? new[] { new T[0] } :
+          elements.SelectMany((e, i) =>
+            elements.Skip(i + 1).Combinations(k - 1).Select(c => (new[] { e }).Concat(c)));
+    }
+}
